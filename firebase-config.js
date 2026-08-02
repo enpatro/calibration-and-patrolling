d3888664388f82818ca4328e83cae8f4a7028fd1
EPatro <71399081+enpatro@users.noreply.github.com>
2026-08-02 17:23:59 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'REPLACE_WITH_API_KEY',
-  authDomain: 'REPLACE_WITH_PROJECT_ID.firebaseapp.com',
-  projectId: 'REPLACE_WITH_PROJECT_ID',
-  storageBucket: 'REPLACE_WITH_PROJECT_ID.appspot.com',
-  messagingSenderId: 'REPLACE_WITH_SENDER_ID',
-  appId: 'REPLACE_WITH_APP_ID'
+  apiKey: 'AIzaSyDqtU3TD9vzoePW39PU4JLQmbdpJT_cf7A',
+  authDomain: 'calibration-1a2d3.firebaseapp.com',
+  projectId: 'calibration-1a2d3',
+  storageBucket: 'calibration-1a2d3.firebasestorage.app',
+  messagingSenderId: '102893884143',
+  appId: '1:102893884143:web:5b9c43ab6af22ca41f411a'
 };
+
