15bb5969458eb834d071928a8eb5aea407c0f5d6
EPatro <71399081+enpatro@users.noreply.github.com>
2026-08-02 12:52:54 +0530

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+// Firebase web config. Replace these values from Firebase Console > Project Settings > Your apps > Web app.
+export const firebaseConfig = {
+  apiKey: "REPLACE_WITH_API_KEY",
+  authDomain: "REPLACE_WITH_PROJECT_ID.firebaseapp.com",
+  projectId: "REPLACE_WITH_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_PROJECT_ID.appspot.com",
+  messagingSenderId: "REPLACE_WITH_SENDER_ID",
+  appId: "REPLACE_WITH_APP_ID"
+};
